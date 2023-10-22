386a6262562367a4a0298e31f7568aac958ca3b2
ssk8 <stinkyskate@pm.me>
2023-10-21 21:44:33 -0500

    add thermometer

---
+++ olded_featherwing_thermometer/README.md
@@ -0,0 +1,5 @@
+# Just a thermometer
+
+simple thermometer using temp probe and adafruit SH1107 featherwing for feather m0 express 
+
+U8G2 library is missing the correct constructor but this works
---
+++ olded_featherwing_thermometer/olded_featherwing_thermometer.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <avr/dtostrf.h>
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 12
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+U8G2_SH1107_128X128_1_HW_I2C u8g2(U8G2_R1, U8X8_PIN_NONE);
+
+
+void setup(void) {
+  sensors.begin();
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+}
+
+void loop(void) {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+  char temp_ca[4];
+  dtostrf(tempC, sizeof(temp_ca), 1, temp_ca);
+  
+  u8g2.firstPage();
+  do {
+    //u8g2.setFont(u8g2_font_helvB24_tf);
+    u8g2.setFont(u8g2_font_logisoso38_tf);
+    u8g2.setCursor(0, 60);
+    u8g2.print(temp_ca);
+    u8g2.print("°C");
+
+  } while ( u8g2.nextPage() );
+  //delay(1000);
+}
