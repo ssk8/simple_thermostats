cd6937d7c668eab38cc44f80907dfcd2704b8b0e
ssk8 <stinkyskate@pm.me>
2023-10-26 15:01:35 -0500

    fix feather shield

--- olded_featherwing_thermometer/olded_featherwing_thermometer.ino
+++ olded_featherwing_thermometer/olded_featherwing_thermometer.ino
@@ -5,12 +5,13 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 12
+#define ONE_WIRE_BUS 5
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-U8G2_SH1107_128X128_1_HW_I2C u8g2(U8G2_R1, U8X8_PIN_NONE);
+//U8G2_SH1107_128X128_1_HW_I2C u8g2(U8G2_R1, U8X8_PIN_NONE);
+U8G2_SSD1306_128X32_UNIVISION_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 
 void setup(void) {
@@ -27,9 +28,10 @@ void loop(void) {
   
   u8g2.firstPage();
   do {
-    //u8g2.setFont(u8g2_font_helvB24_tf);
-    u8g2.setFont(u8g2_font_logisoso38_tf);
-    u8g2.setCursor(0, 60);
+    u8g2.setFont(u8g2_font_helvB24_tf);
+    //u8g2.setFont(u8g2_font_logisoso38_tf);
+    //u8g2.setCursor(0, 60);
+    u8g2.setCursor(0, 32);
     u8g2.print(temp_ca);
     u8g2.print("°C");
 
